dd3f684d742b709e6d7d8ae17905b4d1be32f6de
achoczaj <arkadiusz.choczaj@gmail.com>
2017-08-15 15:20:07 +0100

    refactoring of code

--- MDS_Chapter_1/MDS_Exhibit_1_1.R
+++ MDS_Chapter_1/MDS_Exhibit_1_1.R
@@ -1,15 +1,33 @@
 # Traditional Conjoint Analysis (R)
 
+# Conjoint analysis is a survey-based statistical technique used in market
+# research that helps determine how people value different attributes (feature,
+# function, benefits) that make up an individual product or service.
+# 
+# The objective of conjoint analysis is to determine what combination of a
+# limited number of attributes is most influential on respondent choice or
+# decision making. A controlled set of potential products or services is shown
+# to survey respondents and by analyzing how they make preferences between these
+# products, the implicit valuation of the individual elements making up the
+# product or service can be determined. These implicit valuations (utilities or
+# part-worths) can be used to create market models that estimate market share,
+# revenue and even profitability of new designs.
+
+
 # R preliminaries to get the user-defined function for spine chart: 
-# place the spine chart code file <R_utility_program_1.R>
+# 1. place the spine chart code file <R_utility_program_1.R>
 # in your working directory and execute it by
 #     source("R_utility_program_1.R")
-# Or if you have the R binary file in your working directory, use
+# or if you have the R binary file in your working directory, use
 #     load(file="mtpa_spine_chart.Rdata")
 
 # spine chart accommodates up to 45 part-worths on one page
 # |part-worth| <= 40 can be plotted directly on the spine chart
 # |part-worths| > 40 can be accommodated through standardization
+
+setwd("MDS_Chapter_1")
+
+load(file="mtpa_spine_chart.Rdata")
 
 print.digits <- 2  # set number of digits on print and spine chart
 
@@ -17,19 +35,24 @@ library(support.CEs)  # package for survey construction
 
 # generate a balanced set of product profiles for survey
 provider.survey <- Lma.design(attribute.names = 
-  list(brand = c("AT&T","T-Mobile","US Cellular","Verizon"), 
-  startup = c("$100","$200","$300","$400"), 
-  monthly = c("$100","$200","$300","$400"),
-  service = c("4G NO","4G YES"), 
-  retail = c("Retail NO","Retail YES"),
-  apple = c("Apple NO","Apple YES"), 
-  samsung = c("Samsung NO","Samsung YES"), 
-  google = c("Nexus NO","Nexus YES")), nalternatives = 1, nblocks=1, seed=9999)
-print(questionnaire(provider.survey))  # print survey design for review
+  list(
+    brand = c("AT&T","T-Mobile","US Cellular","Verizon"), 
+    startup = c("$100","$200","$300","$400"), 
+    monthly = c("$100","$200","$300","$400"),
+    service = c("4G NO","4G YES"), 
+    retail = c("Retail NO","Retail YES"),
+    apple = c("Apple NO","Apple YES"), 
+    samsung = c("Samsung NO","Samsung YES"), 
+    google = c("Nexus NO","Nexus YES")), 
+  nalternatives = 1, nblocks=1, seed=9999)
+# print survey design for review
+print(questionnaire(provider.survey))  
 
-sink("questions_for_survey.txt")  # send survey to external text file
+# send survey to external text file
+sink("questions_for_survey.txt")  
 questionnaire(provider.survey)
 sink() # send output back to the screen
+
 
 # user-defined function for plotting descriptive attribute names 
 effect.name.map <- function(effect.name) { 
@@ -45,6 +68,7 @@ effect.name.map <- function(effect.name) {
 
 # read in conjoint survey profiles with respondent ranks
 conjoint.data.frame <- read.csv("mobile_services_ranking.csv")
+
 
 # set up sum contrasts for effects coding as needed for conjoint analysis
 options(contrasts=c("contr.sum","contr.poly"))
@@ -63,10 +87,12 @@ conjoint.results <-
 
 conjoint.results$attributes <- names(conjoint.results$contrasts)
 
+
 # compute and store part-worths in the conjoint.results list structure
 part.worths <- conjoint.results$xlevels  # list of same structure as xlevels
 end.index.for.coefficient <- 1  # intitialize skipping the intercept
 part.worth.vector <- NULL # used for accumulation of part worths
+
 for(index.for.attribute in seq(along=conjoint.results$contrasts)) {
   nlevels <- length(unlist(conjoint.results$xlevels[index.for.attribute]))
   begin.index.for.coefficient <- end.index.for.coefficient + 1
@@ -80,30 +106,39 @@ for(index.for.attribute in seq(along=conjoint.results$contrasts)) {
   part.worth.vector <- 
     c(part.worth.vector,unlist(part.worths[index.for.attribute]))    
   } 
+
 conjoint.results$part.worths <- part.worths
+
 
 # compute standardized part-worths
 standardize <- function(x) {(x - mean(x)) / sd(x)}
 conjoint.results$standardized.part.worths <- 
-  lapply(conjoint.results$part.worths,standardize)
+  lapply(conjoint.results$part.worths, standardize)
  
+
 # compute and store part-worth ranges for each attribute 
 part.worth.ranges <- conjoint.results$contrasts
-for(index.for.attribute in seq(along=conjoint.results$contrasts)) 
+
+for(index.for.attribute in seq(along=conjoint.results$contrasts)) {
   part.worth.ranges[index.for.attribute] <- 
   dist(range(conjoint.results$part.worths[index.for.attribute]))
+  }
+
 conjoint.results$part.worth.ranges <- part.worth.ranges
 
 sum.part.worth.ranges <- sum(as.numeric(conjoint.results$part.worth.ranges))
 
+
 # compute and store importance values for each attribute 
 attribute.importance <- conjoint.results$contrasts
-for(index.for.attribute in seq(along=conjoint.results$contrasts)) 
+for(index.for.attribute in seq(along=conjoint.results$contrasts)) {
   attribute.importance[index.for.attribute] <- 
   (dist(range(conjoint.results$part.worths[index.for.attribute]))/
   sum.part.worth.ranges) * 100
+  }
 conjoint.results$attribute.importance <- attribute.importance
  
+
 # data frame for ordering attribute names
 attribute.name <- names(conjoint.results$contrasts)
 attribute.importance <- as.numeric(attribute.importance)
@@ -115,11 +150,13 @@ conjoint.results$ordered.attributes <-
 # respondent internal consistency added to list structure
 conjoint.results$internal.consistency <- summary(main.effects.model.fit)$r.squared 
  
+
 # user-defined function for printing conjoint measures
 if (print.digits == 2) 
   pretty.print <- function(x) {sprintf("%1.2f",round(x,digits = 2))} 
 if (print.digits == 3) 
   pretty.print <- function(x) {sprintf("%1.3f",round(x,digits = 3))} 
+
  
 # report conjoint measures to console 
 # use pretty.print to provide nicely formated output
@@ -141,6 +178,7 @@ for(k in seq(along=conjoint.results$ordered.attributes)) {
     [conjoint.results$ordered.attributes[k]])))
   }
 
+
 # plotting of spine chart begins here
 # all graphical output is routed to external pdf file
 pdf(file = "fig_preference_mobile_services_results.pdf", width=8.5, height=11)
@@ -150,6 +188,7 @@ dev.off()  # close the graphics output device
 # Suggestions for the student:
 # Enter your own rankings for the product profiles and generate
 # conjoint measures of attribute importance and level part-worths.
+
 # Note that the model fit to the data is a linear main-effects model.
 # See if you can build a model with interaction effects for service
 # provider attributes.
